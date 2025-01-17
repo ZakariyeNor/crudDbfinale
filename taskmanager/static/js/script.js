a9e7810d65c015736f58a1101da8905eed958c20
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-17 12:42:43 +0000

    Add to the new task logic and form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "select"}
+    });
+
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
+
+
   });
+
+  
+    
